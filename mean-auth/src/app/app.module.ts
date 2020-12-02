362b38b33d8486fa8026531d02f216365a4e431e
Jason Lu <jasonlu2907@gmail.com>
2020-12-01 21:02:10 -0600

    Setting up mean-auth angular app

---
+++ mean-auth/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+
+
+import { routing } from './app.routing';
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ProfileComponent,
+    LoginComponent,
+    DashboardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ mean-auth/src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'profile', component: RegisterComponent},
+  {path: '', component: HomeComponent},
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
